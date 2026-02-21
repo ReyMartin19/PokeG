c99afc6d14b52305a3f58370be94153e9a2094ad
Rey <reymartinagluya8@gmail.com>
2026-02-21 20:23:13 +0800

    fix: remove the battle files

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,3 @@ Route::get('/', [PokemonController::class, 'index'])->name('pokedex.index');
 //pokedex
 Route::get('/battle', [PokemonController::class, 'battle'])->name('pokedex.battle');
 Route::get('/cards', [PokemonController::class, 'cards'])->name('pokedex.cards');
-
-//battle
-Route::get('/battle/init', [BattleController::class, 'createQuickMatch'])->name('battle.init');
-Route::get('/battle/arena', function() {
-    return view('pokedex.battle.arena', ['state' => session('battle_state')]);
-})->name('battle.arena');
-Route::post('/battle/attack', [BattleController::class, 'attack'])->name('battle.attack');
